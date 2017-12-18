fc1318abc81c297a7da8803ef8014b36b829d4ff
Emmeggi95 <marco.gullo@mail.polimi.it>
2017-12-18 21:15:07 +0100

    Google Maps improvements

--- Implementation/src/main/java/com/polimi/travlendar/gmaps/JavaScriptMap.java
+++ Implementation/src/main/java/com/polimi/travlendar/gmaps/JavaScriptMap.java
@@ -10,11 +10,11 @@ import com.vaadin.ui.AbstractJavaScriptComponent;
  *
  */
 @SuppressWarnings("serial")
-@JavaScript({"showMap_1.1.js", "https://maps.googleapis.com/maps/api/js?key=AIzaSyDGsAnfiLGU5WG2Yh7_glDJrRYvDnkGsSg&v=3.exp"})
-@StyleSheet({"mapStyle_1.1.css"})
-public class Map extends AbstractJavaScriptComponent {
+@JavaScript({"vaadin://themes/demo/js/showMap_1.1.js", "https://maps.googleapis.com/maps/api/js?key=AIzaSyDGsAnfiLGU5WG2Yh7_glDJrRYvDnkGsSg&v=3.exp"})
+@StyleSheet({"vaadin://themes/demo/css/mapStyle_1.1.css"})
+public class JavaScriptMap extends AbstractJavaScriptComponent {
 
-    public Map(Float lat, Float lon) {
+    public JavaScriptMap(Float lat, Float lon) {
        callFunction("myfunction", lat, lon); //Pass parameters to your function
     }
 }
--- Implementation/src/main/java/com/polimi/travlendar/ui/pages/TestingPage.java
+++ Implementation/src/main/java/com/polimi/travlendar/ui/pages/TestingPage.java
@@ -6,6 +6,7 @@ import com.github.appreciated.app.layout.annotations.MenuCaption;
 import com.github.appreciated.app.layout.annotations.MenuIcon;
 import com.polimi.travlendar.NotificationManager;
 import com.polimi.travlendar.User;
+import com.polimi.travlendar.gmaps.JavaScriptMap;
 import com.polimi.travlendar.ui.AddCardForm;
 import com.polimi.travlendar.ui.pages.gmaps.MapsAddOnPage;
 import com.polimi.travlendar.ui.pages.gmaps.MapsPage;
@@ -21,9 +22,11 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 
 /**
  * Page with debug and testing aim.
+ *
  * @author Emmeggi95
  *
  */
@@ -31,68 +34,77 @@ import com.vaadin.ui.VerticalLayout;
 @SpringView(name = TestingPage.NAME)
 @MenuCaption("Test")
 @MenuIcon(VaadinIcons.TERMINAL)
-public class TestingPage extends VerticalLayout implements View{
-	
-	public static final String NAME = "test";
-	
-	@Autowired
-	private User user;
-	
-	@Autowired
-	private NotificationManager manager;
-	
-	@Autowired
-	private AddCardForm cardForm;
-        
-                
-	
-	@Override
-	public void enter(ViewChangeListener.ViewChangeEvent event) {
-		Button auth = new Button("Authentication");
-		Button map = new Button("Google Maps");
-		Button mapAddon = new Button("Google Maps Addon");
-		Button placeSearch = new Button("Place search");
-		Button scheduleMegaBella = new Button("Schedule più bella di tutte le altre che abbiamo fatto");
-		Button tricky = new Button("Tricky");
-		
-		this.addComponents(new Label("Testing page"), auth, map, mapAddon, placeSearch, scheduleMegaBella, tricky);
-		
-		auth.addClickListener(e -> getUI().getNavigator().navigateTo(AuthenticationPage.NAME));
-		map.addClickListener(e -> getUI().getNavigator().navigateTo(MapsPage.NAME));
-		mapAddon.addClickListener(e -> getUI().getNavigator().navigateTo(MapsAddOnPage.NAME));
-		placeSearch.addClickListener(e -> getUI().getNavigator().navigateTo(PlaceSearchPage.NAME));
-		scheduleMegaBella.addClickListener(e -> getUI().getNavigator().navigateTo(SchedulePage.NAME));
-		tricky.addClickListener(e -> getUI().getNavigator().navigateTo("tricky"));
-		
-		// Session information
-		
-		Label info = new Label();
-		info.setContentMode(ContentMode.HTML);
-		if(VaadinSession.getCurrent().getAttribute("user")==null) {
-			info.setValue("Unlogged user");
-		} else {
-			info.setValue("Current user: " + user.getEmail() + "<br>ID: " + user.getId());
-		}
-		this.addComponent(info);
-		
-		// Testing Vaadin singleton
-		
-		HorizontalLayout managerTesting = new HorizontalLayout();
-		Label explain = new Label("Global message (every session sees the same text): ");
-		TextField input = new TextField();
-		input.setPlaceholder("Enter global message");
-		input.setValue(manager.getText());
-		Button submit = new Button("Submit");
-		Button update = new Button("Update");
-		update.addClickListener(e -> input.setValue(manager.getText()));
-		submit.addClickListener(e -> manager.setText(input.getValue()));
-		managerTesting.addComponents(explain, input, submit, update);
-		this.addComponent(managerTesting);
-		
-		// Payment form
-		
-		this.addComponent(cardForm);
-                                
-	}
+public class TestingPage extends VerticalLayout implements View {
+
+    public static final String NAME = "test";
+
+    @Autowired
+    private User user;
+
+    @Autowired
+    private NotificationManager manager;
+
+    @Autowired
+    private AddCardForm cardForm;
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+        Button auth = new Button("Authentication");
+        Button map = new Button("Google Maps");
+        Button mapAddon = new Button("Google Maps Addon");
+        Button placeSearch = new Button("Place search");
+        Button scheduleMegaBella = new Button("Schedule più bella di tutte le altre che abbiamo fatto");
+        Button tricky = new Button("Tricky");
+
+        this.addComponents(new Label("Testing page"), auth, map, mapAddon, placeSearch, scheduleMegaBella, tricky);
+
+        auth.addClickListener(e -> getUI().getNavigator().navigateTo(AuthenticationPage.NAME));
+        map.addClickListener(e -> getUI().getNavigator().navigateTo(MapsPage.NAME));
+        mapAddon.addClickListener(e -> getUI().getNavigator().navigateTo(MapsAddOnPage.NAME));
+        placeSearch.addClickListener(e -> getUI().getNavigator().navigateTo(PlaceSearchPage.NAME));
+        scheduleMegaBella.addClickListener(e -> getUI().getNavigator().navigateTo(SchedulePage.NAME));
+        tricky.addClickListener(e -> getUI().getNavigator().navigateTo("tricky"));
+
+        // Session information
+        Label info = new Label();
+        info.setContentMode(ContentMode.HTML);
+        if (VaadinSession.getCurrent().getAttribute("user") == null) {
+            info.setValue("Unlogged user");
+        } else {
+            info.setValue("Current user: " + user.getEmail() + "<br>ID: " + user.getId());
+        }
+        this.addComponent(info);
+
+        // Testing Vaadin singleton
+        HorizontalLayout managerTesting = new HorizontalLayout();
+        Label explain = new Label("Global message (every session sees the same text): ");
+        TextField input = new TextField();
+        input.setPlaceholder("Enter global message");
+        input.setValue(manager.getText());
+        Button submit = new Button("Submit");
+        Button update = new Button("Update");
+        update.addClickListener(e -> input.setValue(manager.getText()));
+        submit.addClickListener(e -> manager.setText(input.getValue()));
+        managerTesting.addComponents(explain, input, submit, update);
+        this.addComponent(managerTesting);
+
+        // Payment form
+        this.addComponent(cardForm);
+
+        // JavaScript map
+        Window mapWindow = new Window("JavaScript map");
+        mapWindow.setWidth("600px");
+        mapWindow.setResizable(false);
+
+        Button javaScriptMapButton = new Button("See JavaScript map");
+        javaScriptMapButton.addClickListener(e -> {
+            JavaScriptMap javaScriptMap = new JavaScriptMap(new Float(45.4782167), new Float(9.2250863));
+            javaScriptMap.setId("mapob");
+            mapWindow.setContent(javaScriptMap);
+            getUI().addWindow(mapWindow);
+        });
+
+        this.addComponent(javaScriptMapButton);
+    }
 
 }
